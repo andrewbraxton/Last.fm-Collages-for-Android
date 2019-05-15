23e488ab47cd0b7efc3f762c3291e85869366688
Andrew Braxton <braxton3@illinois.edu>
2019-05-15 16:52:53 -0400

    action bar with overflow menu

--- app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
+++ app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
@@ -2,12 +2,25 @@ package com.andrewbraxton.lastfmcollagesforandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }

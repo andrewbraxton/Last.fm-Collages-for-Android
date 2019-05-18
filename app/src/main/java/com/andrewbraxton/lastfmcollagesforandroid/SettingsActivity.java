8f4fd601c69054c324126d5dd55af78109325b30
Andrew Braxton <braxton3@illinois.edu>
2019-05-18 17:00:15 -0400

    migrate to AndroidX

--- app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/SettingsActivity.java
+++ app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/SettingsActivity.java
@@ -1,9 +1,9 @@
 package com.andrewbraxton.lastfmcollagesforandroid;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
 

0ffc31331b643d46c4f89aa1dbca609bf4005c49
Andrew Braxton <braxton3@illinois.edu>
2019-05-21 17:58:38 -0400

    toast for unentered username

--- app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
+++ app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
@@ -1,14 +1,19 @@
 package com.andrewbraxton.lastfmcollagesforandroid;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void generateImage(View v) {
         Log.d(CLICK_TAG, "Generate");
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String username = prefs.getString(getString(R.string.key_pref_username), null);
+        if (username == null || username.isEmpty()) {
+            Toast.makeText(this, getString(R.string.toast_no_username), Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public void openSettings(MenuItem v) {

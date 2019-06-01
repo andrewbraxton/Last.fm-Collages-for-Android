1793844c09635038f8f42433d3d8c7295f495a41
Andrew Braxton <braxton3@illinois.edu>
2019-06-01 00:22:26 -0400

    split cover art fetching and collage drawing into separate tasks

--- app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
+++ app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
@@ -17,7 +17,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.ClientError;
@@ -34,20 +33,24 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Javadoc
-    // TODO: better logging
-    // TODO: write a save image method
-    // TODO: pull out constants (png, 300)
+    // TODO: pull out constants
+    // TODO: app icon
+    // TODO: persistent collage ImageView
 
     private static final String LOG_TAG = "MainActivityTag";
 
-    private static final String COVERART_DIR = "coverart";
-    private static final String COLLAGE_DIR = "collage";
+    public static final int COVERART_SIZE = 300;
+    private static final String COVERART = "coverart";
+    private static final String COLLAGE = "collage";
+    public static final String PNG = ".png";
+
 
     private RequestQueue queue;
     private final Gson gson = new Gson();
@@ -62,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
 
         queue = Volley.newRequestQueue(this);
 
-        File coverArtDir = new File(getFilesDir(), COVERART_DIR);
-        File collageDir = new File(getFilesDir(), COLLAGE_DIR);
+        File coverArtDir = new File(getFilesDir(), COVERART);
+        File collageDir = new File(getFilesDir(), COLLAGE);
         coverArtDir.mkdir();
         collageDir.mkdir();
     }
@@ -75,44 +78,10 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    // TODO: redesign this so that the ImageView updates cover arts progressively
+
     public void generateButtonClicked(View v) {
         Log.i(LOG_TAG, "Generate button clicked");
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String username = prefs.getString(getString(R.string.key_pref_username), "");
-        int numDays = Integer.parseInt(prefs.getString(getString(R.string.key_pref_date_range), "7"));
-        int collageSize = Integer.parseInt(prefs.getString(getString(R.string.key_pref_size), "3"));
-
-        if (username.isEmpty()) {
-            Toast.makeText(this, getString(R.string.toast_no_username), Toast.LENGTH_SHORT).show();
-            Log.i(LOG_TAG,"Generate error: no username");
-            return;
-        }
-
-        String url = ApiStringBuilder.buildGetAlbumChartUrl(username, getFromDate(numDays), getToDate());
-        StringRequest stringRequest = new StringRequest(
-                url,
-                response -> {
-                    response = response.replace("#", ""); // removing the pound signs in Last.fm's JSON keys
-                    AlbumChart chartObject = gson.fromJson(response, AlbumChart.class);
-                    ImageView chartImage = findViewById(R.id.chartImage);
-                    chartImage.setImageBitmap(generateChartBitmap(chartObject, collageSize));
-                    Toast.makeText(this, getString(R.string.toast_generate_successful), Toast.LENGTH_SHORT).show();
-
-                    Log.i(LOG_TAG,"Generate success");
-                },
-                error -> {
-                    String errorMessage = getString(R.string.toast_network_error);
-                    if (error instanceof ClientError) {
-                        errorMessage = getString(R.string.toast_invalid_username);
-                    }
-                    Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
-
-                    Log.d(LOG_TAG,"Generate error: " + errorMessage);
-                }
-        );
-        queue.add(stringRequest);
+        // TODO: implement
     }
 
     public void settingsButtonClicked(MenuItem v) {
@@ -125,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
     public void shareButtonClicked(MenuItem v) {
         Log.i(LOG_TAG, "Share button clicked");
         // TODO: implement
+
     }
 
     public void downloadButtonClicked(MenuItem v) {
@@ -134,9 +104,147 @@ public class MainActivity extends AppCompatActivity {
 
     // Helper functions.
 
+    private Bitmap createCollageBitmap() {
+        clearCollageDir();
+
+        int collageSize = getCollageSize();
+        Bitmap collage = getBlackBitmap(collageSize);
+        Canvas canvas = new Canvas(collage);
+
+        // draw all cover art onto the canvas
+        int x;
+        int y;
+        for (int i = 0; i < collageSize * collageSize; i++) {
+            x = (i % collageSize) * COVERART_SIZE;
+            y = (i / collageSize) * COVERART_SIZE;
+            File coverArtFile = new File(getCoverArtDir(), i + PNG);
+            if (coverArtFile.exists()) {
+                Bitmap coverArt = BitmapFactory.decodeFile(coverArtFile.getAbsolutePath());
+                canvas.drawBitmap(coverArt, x, y, null);
+            }
+        }
+
+        saveBitmap(new File(getCollageDir(), COLLAGE + PNG), collage);
+        return collage;
+    }
+
+    private void fetchAllCoverArt() {
+        clearCoverArtDir();
+
+        String user = getUsername();
+        int numDays = getNumDays();
+        int collageSize = getCollageSize();
+
+        StringRequest chartRequest = new StringRequest(
+                ApiStringBuilder.buildGetAlbumChartUrl(user, getFromDate(numDays), getToDate()),
+                jsonResponse -> {
+                    Log.i(LOG_TAG, "Fetching cover art...");
+
+                    List<Album> albums = getChartAlbums(jsonResponse, collageSize);
+                    for (int i = 0; i < albums.size(); i++) {
+                        fetchCoverArt(albums.get(i), i + ".png");
+                    }
+                },
+                error -> {
+                    String errorMessage;
+                    if (error instanceof ClientError) {
+                        errorMessage = getString(R.string.toast_invalid_username);
+                        Log.d(LOG_TAG, "StringRequest error: Invalid username");
+                    } else {
+                        errorMessage = getString(R.string.toast_network_error);
+                        Log.d(LOG_TAG, "StringRequest error: Network error");
+                    }
+                    Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+        );
+        queue.add(chartRequest);
+    }
+
+    /**
+     * Fetches the cover art for this album from the Last.fm API. Downloads it to internal storage.
+     *
+     * @param album    the album to get the cover art for
+     * @param filename the name that the downloaded file will have
+     */
+    private void fetchCoverArt(Album album, String filename) {
+        // TODO: make readable
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                ApiStringBuilder.buildGetAlbumInfoUrl(album),
+                null,
+                albumInfo -> {
+                    try {
+                        ImageRequest imageRequest = new ImageRequest(
+                                getLargestImageUrl(albumInfo),
+                                coverArt -> {
+                                    Log.d(LOG_TAG, "Fetch success: " + album);
+                                    saveBitmap(new File(getCoverArtDir(), filename), coverArt);
+                                },
+                                0, 0, null, null,
+                                error -> {
+                                    Log.e(LOG_TAG, "Fetch error (ImageRequest):  " + album);
+                                });
+                        queue.add(imageRequest);
+                    } catch (JSONException e) {
+                        Log.e(LOG_TAG, "Fetch error (No cover art found): " + album);
+                    }
+                },
+                error -> {
+                    Log.e(LOG_TAG, "Fetch error (JsonRequest Error): " + album);
+                });
+        queue.add(jsonObjectRequest);
+    }
+
+    private List<Album> getChartAlbums(String chartJson, int collageSize) {
+        chartJson = chartJson.replace("#", "");
+        AlbumChart chart = gson.fromJson(chartJson, AlbumChart.class);
+        int numAlbums = Math.min(collageSize * collageSize, chart.getAlbums().size());
+        return chart.getAlbums().subList(0, numAlbums);
+    }
+
+    private String getLargestImageUrl(JSONObject albumInfo) throws JSONException {
+        JSONArray imageUrls = albumInfo.getJSONObject("album").getJSONArray("image");
+        JSONObject largestImageObject = imageUrls.getJSONObject(imageUrls.length() - 1);
+        return largestImageObject.getString("#text");
+    }
+
+    private void saveBitmap(File saveLocation, Bitmap bmp) {
+        try {
+            OutputStream fOutStream = new FileOutputStream(saveLocation);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, fOutStream);
+            fOutStream.close();
+            Log.i(LOG_TAG, "Save success: " + saveLocation.getName());
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Save error (IOException): " + saveLocation.getName());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Bitmap getBlackBitmap(int collageSize) {
+        int size = COVERART_SIZE * collageSize;
+        Bitmap blackBmp = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+        blackBmp.eraseColor(Color.BLACK);
+        return blackBmp;
+    }
+
+    private SharedPreferences getPrefs() {
+        return PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    private String getUsername() {
+        return getPrefs().getString(getString(R.string.key_pref_username), "");
+    }
+
+    private int getNumDays() {
+        return Integer.parseInt(getPrefs().getString(getString(R.string.key_pref_num_days), "7"));
+    }
+
+    private int getCollageSize() {
+        return Integer.parseInt(getPrefs().getString(getString(R.string.key_pref_size), "3"));
+    }
+
     /**
      * Returns the initial Unix timestamp for use in the "from" API parameter.
-
+     *
      * @param numDays number of days the collage spans
      */
     private long getFromDate(int numDays) {
@@ -146,110 +254,30 @@ public class MainActivity extends AppCompatActivity {
         return getToDate() - (numDays * 86400); // 86400 seconds in a day
     }
 
-    /** Returns the Unix timestamp of the current moment for use in the "to" API parameter. */
+    /**
+     * Returns the Unix timestamp of the current moment for use in the "to" API parameter.
+     */
     private long getToDate() {
         return System.currentTimeMillis() / 1000L;
     }
 
     /**
-     * Fetches the cover art for this album from the Last.fm API and downloads it to internal storage.
-     *
-     * @param album    the album to get the cover art for
-     * @param filename the name that the downloaded file will have
+     * Returns the directory in internal storage where album cover arts are stored.
      */
-    private void fetchCoverArt(Album album, String filename) {
-        // TODO: implement actual error handling
-        // TODO: make readable
-        String albumInfoUrl = ApiStringBuilder.buildGetAlbumInfoUrl(album);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                albumInfoUrl,
-                null,
-                response -> {
-                    try {
-                        JSONArray imageUrls = response.getJSONObject("album").getJSONArray("image");
-                        JSONObject largestImageObject = imageUrls.getJSONObject(imageUrls.length()-1);
-                        String largestImageUrl = largestImageObject.getString("#text");
-                        ImageRequest imageRequest = new ImageRequest(
-                                largestImageUrl,
-                                bitmap -> {
-                                    try {
-                                        Log.d(LOG_TAG, "Fetch success: " + album);
-
-                                        File imageFile = new File(getCoverArtDir(), filename);
-                                        OutputStream fOutStream = new FileOutputStream(imageFile);
-                                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOutStream);
-                                        fOutStream.close();
-                                    } catch (Exception e) {
-                                        Log.e(LOG_TAG, "Fetch error (ImageRequest Error1): " + album);
-                                    }
-                                }, 0, 0, null, null,
-                                error -> {
-                                    Log.e(LOG_TAG, "Fetch error (ImageRequest Error2): " + album);
-                                });
-                        queue.add(imageRequest);
-                    } catch (JSONException e) {
-                        // album couldn't be found
-                        Log.e(LOG_TAG, "Fetch error (JsonRequest Error1): " + album);
-                    }
-                },
-                error -> {
-                    Log.e(LOG_TAG, "Fetch error (JsonRequest Error2): " + album);
-                });
-        queue.add(jsonObjectRequest);
-    }
-
-    private Bitmap generateChartBitmap(AlbumChart chartObject, int collageSize) {
-        //clearCoverArtDir();
-        List<Album> albums = chartObject.getAlbums();
-        int numAlbums = collageSize * collageSize;
-        for (int i = 0; i < numAlbums; i++) {
-            fetchCoverArt(albums.get(i), i + ".png");
-        }
-
-        // set up black canvas to draw on
-        int dimension = 300 * collageSize;
-        Bitmap collage = Bitmap.createBitmap(dimension, dimension, Bitmap.Config.ARGB_8888);
-        collage.eraseColor(Color.BLACK);
-        Canvas canvas = new Canvas(collage);
-
-        // draw the cover arts onto the canvas
-        int x = 0;
-        int y = 0;
-        for (int i = 0; i < numAlbums; i++) {
-            x = (i % collageSize) * 300;
-            if (i > 0 && i % collageSize == 0) {
-                y += 300;
-            }
-            String filename = i + ".png";
-            File coverArtFile = new File(getCoverArtDir(), filename);
-            if (coverArtFile.exists()) {
-                Bitmap coverArt = BitmapFactory.decodeFile(coverArtFile.getAbsolutePath());
-                canvas.drawBitmap(coverArt, x, y, null);
-            }
-        }
-
-        // save collage to internal storage
-        try {
-            File imageFile = new File(getCollageDir(), "collage.png");
-            OutputStream fOutStream = new FileOutputStream(imageFile);
-            collage.compress(Bitmap.CompressFormat.PNG, 100, fOutStream);
-            fOutStream.close();
-        } catch (Exception e) {}
-
-        return collage;
-    }
-
-    /** Returns the directory in internal storage where album cover arts are stored. */
     private File getCoverArtDir() {
-        return new File(getFilesDir(), COVERART_DIR);
+        return new File(getFilesDir(), COVERART);
     }
 
-    /** Returns the directory in internal storage where generated collages are stored. */
+    /**
+     * Returns the directory in internal storage where generated collages are stored.
+     */
     private File getCollageDir() {
-        return new File(getFilesDir(), COLLAGE_DIR);
+        return new File(getFilesDir(), COLLAGE);
     }
 
-    /** Clears the cover art directory in internal storage. */
+    /**
+     * Clears the cover art directory in internal storage.
+     */
     private void clearCoverArtDir() {
         for (File coverArt : getCoverArtDir().listFiles()) {
             coverArt.delete();
@@ -257,12 +285,13 @@ public class MainActivity extends AppCompatActivity {
         Log.i(LOG_TAG, "Cover art directory cleared");
     }
 
-    /** Clears the collage directory in internal storage. */
+    /**
+     * Clears the collage directory in internal storage.
+     */
     private void clearCollageDir() {
         for (File collage : getCollageDir().listFiles()) {
             collage.delete();
         }
         Log.i(LOG_TAG, "Collage directory cleared");
     }
-
 }

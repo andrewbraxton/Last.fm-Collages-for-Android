c93a3e536732892652d55bcb0cc1b49cfa7d658c
Andrew Braxton <braxton3@illinois.edu>
2019-06-01 17:14:30 -0400

    collage generation functional!

--- app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
+++ app/src/main/java/com/andrewbraxton/lastfmcollagesforandroid/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.ClientError;
@@ -40,9 +41,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Javadoc
-    // TODO: pull out constants
     // TODO: app icon
     // TODO: persistent collage ImageView
+    // TODO: look into problem with album names containing ampersands, ñ, etc.
 
     private static final String LOG_TAG = "MainActivityTag";
 
@@ -78,10 +79,15 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
     public void generateButtonClicked(View v) {
         Log.i(LOG_TAG, "Generate button clicked");
-        // TODO: implement
+
+        generateCollage();
+    }
+
+    private boolean doneFetchingCoverArt() {
+        // TODO: handle user not having enough albums
+        return getCoverArtDir().listFiles().length == getCollageSize()*getCollageSize();
     }
 
     public void settingsButtonClicked(MenuItem v) {
@@ -102,6 +108,81 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Helper functions.
+
+    private void generateCollage() {
+        clearCoverArtDir();
+
+        String user = getUsername();
+        int numDays = getNumDays();
+        int collageSize = getCollageSize();
+
+        StringRequest chartRequest = new StringRequest(
+                ApiStringBuilder.buildGetAlbumChartUrl(user, getFromDate(numDays), getToDate()),
+                jsonResponse -> {
+                    Log.i(LOG_TAG, "Fetching cover art...");
+
+                    List<Album> albums = getChartAlbums(jsonResponse, collageSize);
+                    for (int i = 0; i < albums.size(); i++) {
+                        fetchCoverArt(albums.get(i), i + PNG);
+                    }
+                },
+                error -> {
+                    String errorMessage;
+                    if (error instanceof ClientError) {
+                        Log.d(LOG_TAG, "StringRequest error: Invalid username");
+                        errorMessage = getString(R.string.toast_invalid_username);
+                    } else {
+                        Log.d(LOG_TAG, "StringRequest error: Network error");
+                        errorMessage = getString(R.string.toast_network_error);
+                    }
+                    Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+        );
+        queue.add(chartRequest);
+    }
+
+    /**
+     * Fetches the cover art for this album from the Last.fm API. Downloads it to internal storage.
+     *
+     * @param album    the album to get the cover art for
+     * @param filename the name that the downloaded file will have
+     */
+    private void fetchCoverArt(Album album, String filename) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                ApiStringBuilder.buildGetAlbumInfoUrl(album),
+                null,
+                albumInfo -> {
+                    File saveLocation = new File(getCoverArtDir(), filename);
+                    try {
+                        ImageRequest imageRequest = new ImageRequest(
+                                getLargestImageUrl(albumInfo),
+                                coverArt -> {
+                                    Log.d(LOG_TAG, "Fetch success: " + album);
+
+                                    saveCoverArt(saveLocation, coverArt);
+                                    displayCollageIfReady();
+                                },
+                                0, 0, null, null,
+                                error -> {
+                                    // TODO: improve
+                                    Log.e(LOG_TAG, "Fetch error (ImageRequest):  " + album);
+
+                                    saveCoverArt(saveLocation, null);
+                                    displayCollageIfReady();
+                                });
+                        queue.add(imageRequest);
+                    } catch (JSONException e) {
+                        Log.e(LOG_TAG, "Fetch error (No cover art found): " + album);
+
+                        saveCoverArt(saveLocation, null);
+                        displayCollageIfReady();
+                    }
+                },
+                error -> {
+                    Log.e(LOG_TAG, "Fetch error (JsonRequest Error): " + album);
+                });
+        queue.add(jsonObjectRequest);
+    }
 
     private Bitmap createCollageBitmap() {
         clearCollageDir();
@@ -125,86 +206,23 @@ public class MainActivity extends AppCompatActivity {
         return collage;
     }
 
-    private void fetchAllCoverArt() {
-        clearCoverArtDir();
+    private void displayCollageIfReady() {
+        if (doneFetchingCoverArt()) {
+            ImageView collageImage = findViewById(R.id.chartImage);
+            collageImage.setImageBitmap(createCollageBitmap());
+            Toast.makeText(this, R.string.toast_generate_successful, Toast.LENGTH_SHORT).show();
 
-        String user = getUsername();
-        int numDays = getNumDays();
-        int collageSize = getCollageSize();
-
-        StringRequest chartRequest = new StringRequest(
-                ApiStringBuilder.buildGetAlbumChartUrl(user, getFromDate(numDays), getToDate()),
-                jsonResponse -> {
-                    Log.i(LOG_TAG, "Fetching cover art...");
-
-                    List<Album> albums = getChartAlbums(jsonResponse, collageSize);
-                    for (int i = 0; i < albums.size(); i++) {
-                        fetchCoverArt(albums.get(i), i + PNG);
-                    }
-                },
-                error -> {
-                    String errorMessage;
-                    if (error instanceof ClientError) {
-                        errorMessage = getString(R.string.toast_invalid_username);
-                        Log.d(LOG_TAG, "StringRequest error: Invalid username");
-                    } else {
-                        errorMessage = getString(R.string.toast_network_error);
-                        Log.d(LOG_TAG, "StringRequest error: Network error");
-                    }
-                    Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
-                }
-        );
-        queue.add(chartRequest);
+            Log.i(LOG_TAG, "Collage displayed");
+        }
     }
 
-    /**
-     * Fetches the cover art for this album from the Last.fm API. Downloads it to internal storage.
-     *
-     * @param album    the album to get the cover art for
-     * @param filename the name that the downloaded file will have
-     */
-    private void fetchCoverArt(Album album, String filename) {
-        // TODO: make readable
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                ApiStringBuilder.buildGetAlbumInfoUrl(album),
-                null,
-                albumInfo -> {
-                    File saveLocation = new File(getCoverArtDir(), filename);
-                    try {
-                        ImageRequest imageRequest = new ImageRequest(
-                                getLargestImageUrl(albumInfo),
-                                coverArt -> {
-                                    Log.d(LOG_TAG, "Fetch success: " + album);
-                                    saveBitmap(saveLocation, coverArt);
-                                },
-                                0, 0, null, null,
-                                error -> {
-                                    Log.e(LOG_TAG, "Fetch error (ImageRequest):  " + album);
-                                });
-                        queue.add(imageRequest);
-                    } catch (JSONException e) {
-                        Log.e(LOG_TAG, "Fetch error (No cover art found): " + album);
-                        saveBitmap(saveLocation, getBlackBitmap(COVERART_SIZE));
-                        // TODO: better handling of no cover art
-                    }
-                },
-                error -> {
-                    Log.e(LOG_TAG, "Fetch error (JsonRequest Error): " + album);
-                });
-        queue.add(jsonObjectRequest);
-    }
-
-    private List<Album> getChartAlbums(String chartJson, int collageSize) {
-        chartJson = chartJson.replace("#", "");
-        AlbumChart chart = gson.fromJson(chartJson, AlbumChart.class);
-        int numAlbums = Math.min(collageSize * collageSize, chart.getAlbums().size());
-        return chart.getAlbums().subList(0, numAlbums);
-    }
-
-    private String getLargestImageUrl(JSONObject albumInfo) throws JSONException {
-        JSONArray imageUrls = albumInfo.getJSONObject("album").getJSONArray("image");
-        JSONObject largestImageObject = imageUrls.getJSONObject(imageUrls.length() - 1);
-        return largestImageObject.getString("#text");
+    private void saveCoverArt(File saveLocation, Bitmap coverArt) {
+        // TODO: improve this
+        if (coverArt == null) {
+            saveBitmap(saveLocation, getBlackBitmap(COVERART_SIZE));
+        } else {
+            saveBitmap(saveLocation, coverArt);
+        }
     }
 
     private void saveBitmap(File saveLocation, Bitmap bmp) {
@@ -223,6 +241,19 @@ public class MainActivity extends AppCompatActivity {
         Bitmap blackBmp = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
         blackBmp.eraseColor(Color.BLACK);
         return blackBmp;
+    }
+
+    private List<Album> getChartAlbums(String chartJson, int collageSize) {
+        chartJson = chartJson.replace("#", "");
+        AlbumChart chart = gson.fromJson(chartJson, AlbumChart.class);
+        int numAlbums = Math.min(collageSize * collageSize, chart.getAlbums().size());
+        return chart.getAlbums().subList(0, numAlbums);
+    }
+
+    private String getLargestImageUrl(JSONObject albumInfo) throws JSONException {
+        JSONArray imageUrls = albumInfo.getJSONObject("album").getJSONArray("image");
+        JSONObject largestImage = imageUrls.getJSONObject(imageUrls.length() - 1);
+        return largestImage.getString("#text");
     }
 
     private SharedPreferences getPrefs() {
